26855d7266e0aac44a3551285039048dc60a340d
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-04-14 16:01:51 -0300

    recuperar objetos serializados

--- src/serializando/Serializando.java
+++ src/serializando/Serializando.java
@@ -1,5 +1,11 @@
 package serializando;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -8,11 +14,53 @@ public class Serializando {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Administrador jefe = new Administrador("pablo", 25000, 2012, 05, 01);
+		jefe.setIncentivo(5000);
+
+		// en este ejemplo el array personal va a ser serializado
+		Empleado[] personal = new Empleado[3];
+		personal[0] = jefe;
+		personal[1] = new Empleado("ana", 15000, 2015, 07, 12);
+		personal[2] = new Empleado("marcelo", 20000, 2015, 05, 01);
+
+		// serializacion:
+		String ruta_almacenamiento = "C:\\Users\\Usuario\\eclipse-workspace\\Acceso_Ficheros\\src\\serializando\\empleado.dat";
+
+		FileOutputStream salida;
+		try {
+			salida = new FileOutputStream(ruta_almacenamiento);
+
+			// flujo de datos de salida (Stream)
+			ObjectOutputStream fichero = new ObjectOutputStream(salida);
+
+			// escritura del objeto ya serializado
+			fichero.writeObject(personal);
+
+			fichero.close();
+
+			// recuprando el objeto serializado:
+			FileInputStream entrada = new FileInputStream(ruta_almacenamiento);
+			ObjectInputStream leyendo = new ObjectInputStream(entrada);
+
+			// aca se obtiene lo guardado
+			Empleado[] personal_recuperado = (Empleado[]) leyendo.readObject();
+
+			leyendo.close();
+
+			for (Empleado un_empleado : personal_recuperado) {
+				System.out.println(un_empleado.toString());
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
 
-class Empleado {
+// implementa la interface Serializable
+class Empleado implements Serializable {
 	private String nombre;
 	private double sueldo;
 	private Date fechaContrato;
